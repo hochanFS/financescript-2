2e168ff04f33d50e675e11095ed4f0a452789b06
Hochan Lee <hochan.lee@financescript.com>
2019-12-22 17:22:43 -0500

    Added IndexController #3

---
+++ src/main/java/com/financescript/springapp/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package com.financescript.springapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping({"", "/", "/index", "/index.html"})
+    public String index()
+    {
+        return "index";
+    }
+}
---
+++ src/test/java/com/financescript/springapp/controllers/IndexControllerTest.java
@@ -0,0 +1,27 @@
+package com.financescript.springapp.controllers;
+
+import org.junit.jupiter.api.*;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+class IndexControllerTest {
+
+    IndexController controller;
+
+    @BeforeEach
+    void setUp() {
+        controller = new IndexController();
+    }
+
+    @Test
+    void index() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+}
