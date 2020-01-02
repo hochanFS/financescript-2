74f18044bf7b939bae5996e7bffdaa5965258457
Hochan Lee <hochan.lee@financescript.com>
2020-01-01 23:36:42 -0500

    Done with java.security.Principal Removed SessionInfo since it is no longer needed. #32

--- src/main/java/com/financescript/springapp/controllers/IndexController.java
+++ src/main/java/com/financescript/springapp/controllers/IndexController.java
@@ -1,13 +1,22 @@
 package com.financescript.springapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Controller
 public class IndexController {
+
     @GetMapping({"", "/", "/index", "/index.html"})
-    public String index()
+    public String index(Principal principal, Model model)
     {
+        if (principal == null) {
+            model.addAttribute("username", null);
+        } else {
+            model.addAttribute("username", principal.getName());
+        }
         return "index";
     }
 }
