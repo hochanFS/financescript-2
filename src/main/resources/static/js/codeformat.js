cd419942722046cda8323a6a784bb05402c618f5
Hochan Lee <hochan.lee@financescript.com>
2020-01-19 23:37:46 -0500

    Add code formatting #42

---
+++ src/main/resources/static/js/codeformat.js
@@ -0,0 +1,32 @@
+var editor_java = document.querySelectorAll(".language_java");
+editor_java.forEach(function(editorElem) {
+    var editor_java = ace.edit(editorElem);
+    editor_java.setTheme("ace/theme/twilight");
+    editor_java.setOptions({
+        maxLines: Infinity
+    });
+    editor_java.setReadOnly(true);
+    editor_java.getSession().setMode("ace/mode/java");
+});
+
+var editor_cpp = document.querySelectorAll(".language_cpp");
+editor_cpp.forEach(function(editorElem) {
+    var editor_cpp = ace.edit(editorElem);
+    editor_cpp.setTheme("ace/theme/twilight");
+    editor_cpp.setOptions({
+        maxLines: Infinity
+    });
+    editor_cpp.setReadOnly(true);
+    editor_cpp.getSession().setMode("ace/mode/cpp");
+});
+
+var editor_python = document.querySelectorAll(".language_python");
+editor_python.forEach(function(editorElem) {
+    var editor_python = ace.edit(editorElem);
+    editor_python.setTheme("ace/theme/twilight");
+    editor_python.setOptions({
+        maxLines: Infinity
+    });
+    editor_python.setReadOnly(true);
+    editor_python.getSession().setMode("ace/mode/python");
+});
