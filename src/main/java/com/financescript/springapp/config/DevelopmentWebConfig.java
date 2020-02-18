7faba469bad8f4906ae71431e50b6ee0e47b2560
Hochan Lee <hochan.lee@financescript.com>
2020-02-17 20:07:17 -0500

    Added static resource path to DevelopmentWebConfig class #60

--- src/main/java/com/financescript/springapp/config/DevelopmentWebConfig.java
+++ src/main/java/com/financescript/springapp/config/DevelopmentWebConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Profile("development")
@@ -13,5 +14,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableAsync
 @EnableScheduling
 public class DevelopmentWebConfig implements WebMvcConfigurer {
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/static/"};
 
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
 }
